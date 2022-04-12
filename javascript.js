4a6d797e6e6ded7b9d85f03b959b5609ed890a92
mochiellee <chenmi399@gmail.com>
2022-04-12 16:18:30 -0400

    Included UI to play game with buttons

--- javascript.js
+++ javascript.js
@@ -17,36 +17,49 @@ function playRound(playerSelection, computerSelection){
 
     if (playerSelection == computerSelection ){
         console.log("It's a tie!");
+        result.textContent = "It's a tie!";
         return "tie";
         //return "It's a tie!";
     }
     else if (playerSelection == "rock" && computerSelection == "paper"){
         console.log("You lose! Paper beats rock.");
+        result.textContent = "You lose! Paper beats rock.";
+        computer.textContent++;
         return "lose";
         //return "You lose! Paper beats rock.";
     }
     else if (playerSelection == "rock" && computerSelection == "scissor"){
         console.log("You win! Rock beats scissor.");
+        result.textContent = "You win! Rock beats scissor.";
+        player.textContent++;
         return "win";
         //return "You win! Rock beats scissor.";
     }
     else if (playerSelection == "paper" && computerSelection == "rock"){
         console.log("You win! Paper beats rock.");
+        result.textContent = "You win! Paper beats rock.";
+        player.textContent++;
         return "win";
         //return "You win! Paper beats rock.";
     }
     else if (playerSelection == "paper" && computerSelection == "scissor"){
         console.log("You lose! Scissor beats paper.");
+        result.textContent = "You lose! Scissor beats paper.";
+        computer.textContent++;
         return "lose";
         //return "You lose! Scissor beats paper.";
     }
     else if (playerSelection == "scissor" && computerSelection == "paper"){
         console.log("You win! Scissor beats paper.");
+        result.textContent = "You win! Scissor beats paper.";
+        player.textContent++;
         return "win";
         //return "You win! Scissor beats paper.";
     }
     else if (playerSelection == "scissor" && computerSelection == "rock"){
         console.log("You lose! Rock beats scissor.");
+        result.textContent = "You lose! Rock beats scissor.";
+        computer.textContent++;
         return "lose";
         //return "You lose! Rock beats scissor."
     }
@@ -55,7 +68,7 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
+/*function game(){
 
     let tie=0;
     let win=0;
@@ -88,4 +101,44 @@ function game(){
     }
 }
 
+
 console.log(game());
+*/
+const result = document.getElementById('result');
+const player = document.getElementById('playerScore');
+const computer = document.getElementById('computerScore');
+const winner = document.getElementById('winner');
+
+
+
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id,computerPlay());
+
+        if (player.textContent == 5){
+            winner.textContent = "You won!Refresh to play again.";
+            document.getElementById('rock').disabled = true;
+            document.getElementById('paper').disabled = true;
+            document.getElementById('scissor').disabled = true;
+        }
+        else if (computer.textContent == 5){
+            winner.textContent = "Computer won!Refresh to play again";
+            document.getElementById('rock').disabled = true;
+            document.getElementById('paper').disabled = true;
+            document.getElementById('scissor').disabled = true;
+        }
+    });
+});
+
+
+
+//return score function
+//if playround returns win, player score ++
+//if playround returns lose, computer score ++
+//if player score of computer score = 5, announce winner
+
+//check winner function
+//if player ==5, player is winner
+//if computer == 5, computer is winner 
